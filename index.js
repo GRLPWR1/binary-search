32ce9ecec5dd68eada0833d9da6a7b7781970025
GRLPWR1 <110125337+GRLPWR1@users.noreply.github.com>
2023-04-24 04:34:14 +0300

    binary search executed, tests passed

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 // Этот метод должен искать элемент в массиве и возвращать его индекс или -1, если элемент не находится в массиве.
 // Например, если ты ищешь G в массиве [A, B, C, D, E, F, G, H], твой метод должен возвращать 6. Если ты ищешь X, он должен возвращать -1.
 
-function binarySearch (){
-
+function binarySearch (arr, el) {
+    let left = -1;
+    let right = arr.length;
+    while (right - left > 1) {
+        let mid = Math.floor((left + right)/2);
+        if (el === arr[mid]) return mid;
+        if (el < arr[mid]) {
+            right = mid;
+        } else {
+            left = mid;
+        }
+    }
+    return -1;
 }
 
 module.exports = binarySearch
--- spec/binarysearch.spec.js
+++ spec/binarysearch.spec.js
@@ -1,13 +1,18 @@
 const binarySearch = require('../index')
 
-test('Функция должна возвращать индекс элемента', () => {
-    
-})
-
-test('Функция должна быть определена', () => {
-    
-})
-
-test('Функция должна возвращать -1, если элемента не существует в данном массиве', () => {
-    
-})
+describe("binarySearch()", function() {
+  test('Функция должна возвращать индекс элемента', () => {
+    expect(binarySearch([0, 1], 1)).toBe(1);
+    expect(binarySearch(['A', 'B', 'C', 'D', 'E', 'F'], 'F')).toBe(5);
+  })
+  
+  test('Функция должна быть определена', () => {
+    expect(binarySearch).toBeDefined();
+    expect(binarySearch).not.toBeUndefined();
+  })
+  
+  test('Функция должна возвращать -1, если элемента не существует в данном массиве', () => {
+    expect(binarySearch([0, 1], 2)).toBe(-1);
+    expect(binarySearch(['A', 'B', 'C'], 'F')).toBe(-1);
+  })
+  });
